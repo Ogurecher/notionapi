d9e61e6d8c456b8adc6c6929738b981c96a21518
Dmitrii Peredreev <ddm157@yandex.ru>
2023-08-02 16:24:03 +0300

    Update client.go

--- client.go
+++ client.go
@@ -108,8 +108,7 @@ repeatRequest:
 	req.Header.Set("User-Agent", userAgent)
 	req.Header.Set("Accept-Language", acceptLang)
 	if c.AuthToken != "" {
-		req.Header.Set("cookie", fmt.Sprintf("token_v2=%v", c.AuthToken))
-		req.Header.Set("cookie", fmt.Sprintf("file_token=%v", c.FileToken))
+		req.Header.Set("cookie", fmt.Sprintf("token_v2=%s;file_token=%s", c.AuthToken, c.FileToken))
 	}
 	var rsp *http.Response
 
